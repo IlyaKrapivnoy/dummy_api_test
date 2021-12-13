0b6545e300ccc63243a5a6964e54518ac048eda8
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-13 14:47:51 +0200

    add Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from './components/Header';
 
 function App() {
-    return <div className='App'>test</div>;
+    return (
+        <div>
+            <CssBaseline />
+            <Header />
+        </div>
+    );
 }
 
 export default App;
