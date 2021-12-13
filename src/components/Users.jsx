1edb653c88f287d4c9af13b6fa8413e005badf43
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-13 16:44:12 +0200

    style user cards

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,29 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 200,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px 10px 10px',
+    },
+    media: {
+        height: 80,
+        width: 80,
+        borderRadius: '50%',
+    },
+});
 
 const Users = () => {
+    const classes = useStyles();
+
     useEffect(() => {
         const APP_ID = '61b724480aaff9678bd56847';
         axios
@@ -20,14 +42,20 @@ const Users = () => {
     return (
         <>
             {console.log(getUsers)}
-            <ul>
-                {getUsers.map((user) => (
-                    <li key={user.id}>
-                        <img src={user.picture} alt='user' />
-                        {user.firstName} {user.lastName}
-                    </li>
-                ))}
-            </ul>
+            {getUsers.map((user) => (
+                <Card className={classes.root} key={user.id}>
+                    <CardMedia
+                        className={classes.media}
+                        image={user.picture}
+                        title='Contemplative Reptile'
+                    />
+                    <CardContent>
+                        <Typography variant='subtitle1'>
+                            {user.firstName} {user.lastName}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ))}
         </>
     );
 };
