186cd89be18a3f6b5a05174cdce7eb0f14f9f982
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-13 18:59:29 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
- 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const defaultState = {
+    users: [],
+};
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'ADD_USERS':
+            return { ...state, users: state.users + action.payload };
+
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
