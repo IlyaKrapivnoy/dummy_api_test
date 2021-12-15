7c5ca4817a43262931c23928d6a1b3b323d80677
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-12-15 10:58:44 +0200

    change imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 import GroupIcon from '@material-ui/icons/Group';
 
 const useStyles = makeStyles({
