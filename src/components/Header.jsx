c4f7c74ddc4eb71f1cd3360f6715b77d5cbebc7a
Ivan Kabakohlo <ivan.kabakohlo@gmail.com>
2021-12-16 23:52:29 +0200

    Add link to /users in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Link } from '@material-ui/core';
+import { NavLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import GroupIcon from '@material-ui/icons/Group';
 
@@ -12,6 +13,12 @@ const useStyles = makeStyles({
         margin: '0 14px',
         letterSpacing: 2,
     },
+    link: {
+        display: 'flex',
+        alignItems: 'center',
+        color: 'white',
+        textDecoration: 'none',
+    }
 });
 
 const Header = () => {
@@ -21,10 +28,12 @@ const Header = () => {
         <div className={classes.root}>
             <AppBar position="fixed">
                 <Toolbar>
-                    <GroupIcon />
-                    <Typography variant="h6" className={classes.title}>
-                      USERS
-                    </Typography>
+                    <NavLink to="/users" as={Link} className={classes.link}>
+                        <GroupIcon />
+                        <Typography variant="h6" className={classes.title}>
+                        USERS
+                        </Typography>
+                    </NavLink>
                 </Toolbar>
             </AppBar>
         </div>
