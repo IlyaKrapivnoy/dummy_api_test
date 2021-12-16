c4f5f63f6531bcccf77259ff94afccc97da46f05
Ivan Kabakohlo <ivan.kabakohlo@gmail.com>
2021-12-16 23:33:30 +0200

    Add redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,21 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
-// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import userReducer from './userDucks'
 import usersReducer from './usersDucks'
+
+const composeEnhancers =
+  (process.env.NODE_ENV !== 'production' &&
+    typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose
 
 const rootReducer = combineReducers({
     users: usersReducer,
     user: userReducer,
 })
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk),
+    ),
+)
